9fe03de28e1c3af5988d2be85570a830a8bb074e
sweetjoohyun <joohyun@sqisoft.com>
2022-07-08 15:09:47 +0900

    업로드 대상 이미지 jpg로 저장 업로드 대상 영상 h264, mp4로 저장

--- toy_backend/common/util.py
+++ toy_backend/common/util.py
@@ -18,46 +18,29 @@ def assign_file_name(original_path, extension):
     file_name = str(random.randint(0, 3)) # 100000
     if is_file_exist(os.path.join(original_path, file_name + extension)):
         _, file_name = assign_file_name(original_path, extension)
-    return os.path.join(original_path, file_name), file_name
+    return original_path, file_name
 
 
-def save_to_jpg(file, file_path):
+def save_to_jpg(file, file_path, file_name):
     extension = '.jpg'
     np_img = np.frombuffer(file.read(), np.uint8)
     img = cv2.imdecode(np_img, cv2.IMREAD_UNCHANGED)
-    file_full_path = file_path + extension
+    file_full_path = os.path.join(file_path, file_name + extension)
     cv2.imwrite(file_full_path, img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
 
 def save_to_mp4(file, file_path, file_name):
-    # np_img = np.frombuffer(file.read(), np.uint8)
-    # img = cv2.imdecode(np_img, cv2.IMREAD_UNCHANGED)
-    # extension = '.jpg'
-    # file_full_path = file_path + extension
-    # cv2.imwrite(file_full_path, img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-    change_video_codec(file, file_path)
-    pass
+    change_video_codec(file, file_path, file_name)
 
 
-def change_video_codec(file, temp_path):
+def change_video_codec(file, file_path, file_name):
     extension = '.mp4'
-    file.save(temp_path + extension)
-    if is_file_exist(temp_path + extension):
-        print(temp_path + extension)
-        probe = ffmpeg.probe(temp_path + extension)
-        if not probe['streams'][0]['codec_name'] == 'h264':
-            temp_filename = file.filename
-            (
-                ffmpeg.input(temp_path)
-                      .output(os.path.join(temp_path, 'temp.mp4'), vcodec='libx264', acodec='aac')
-                      .run(quiet=True)
-            )
-            print(f'temp_filename : {temp_filename}')
-            print(f'temp_path : {temp_path}')
-            os.remove(temp_path + extension)
-            os.rename(os.path.join(temp_path, temp_filename), temp_path + extension)
-
-
-
-
-
+    temp_path = os.path.join(file_path, file_name + extension)
+    file.save(temp_path)
+    probe = ffmpeg.probe(temp_path)
+    if not probe['streams'][0]['codec_name'] == 'h264':
+        ffmpeg.input(temp_path)\
+            .output(os.path.join(file_path, 'temp.mp4'), vcodec='libx264')\
+            .run(quiet=True)
+        os.remove(temp_path)
+        os.rename(os.path.join(file_path, 'temp.mp4'), temp_path)
