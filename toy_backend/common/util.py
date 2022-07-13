c0d0f2d67374e81f324316184e7b57b360eb5ea5
sweetjoohyun <joohyun@sqisoft.com>
2022-07-13 18:35:15 +0900

    경로에서 파일명, 날짜 추출 함수 추가 imdecode에서 cv2.IMREAD_UNCHANGED -> cv2.IMREAD_COLOR 변경

--- toy_backend/common/util.py
+++ toy_backend/common/util.py
@@ -10,6 +10,16 @@ def create_dir(dir_name):
         os.makedirs(dir_name, exist_ok=True)
 
 
+def get_date_from_file_path(input_path):
+    file_path = os.path.dirname(input_path)
+    return os.path.basename(file_path)
+
+
+def split_file_name(input_path):
+    file_name = os.path.basename(input_path)
+    return os.path.splitext(file_name)
+
+
 def is_file_exist(file_dir):
     return os.path.isfile(file_dir)
 
@@ -24,7 +34,7 @@ def assign_file_name(original_path, extension):
 def save_to_jpg(file, file_path, file_name):
     extension = '.jpg'
     np_img = np.frombuffer(file.read(), np.uint8)
-    img = cv2.imdecode(np_img, cv2.IMREAD_UNCHANGED)
+    img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
     file_full_path = os.path.join(file_path, file_name + extension)
     cv2.imwrite(file_full_path, img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
     return file_full_path
