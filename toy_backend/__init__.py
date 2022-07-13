635e34fc6d50e2be816cd543e9abb4273fee879c
sweetjoohyun <joohyun@sqisoft.com>
2022-07-13 18:27:06 +0900

    logger name 변경

--- toy_backend/__init__.py
+++ toy_backend/__init__.py
@@ -3,6 +3,5 @@ from config.config import Config
 from config.logger import get_logger
 
 config = Config(config_file='config.json')
-logger = get_logger(logger_name='toy-logger', logging_config=config.logging)
-parents_dir = os.path.join(config.output_basedir, __name__)
-logger.info(config)
+logger = get_logger(logger_name='toy-backend', logging_config=config.logging)
+logger.debug(config)
