9c254aee0086b816a22c7f9389e4cbdaea796b77
sweetjoohyun <joohyun@sqisoft.com>
2022-07-18 14:21:08 +0900

    SsdMobilenetV2는 ObjectDetectionModel를 상속받는다

--- toy_backend/tf_hub/object_detection/ssd_mobilenet_v2.py
+++ toy_backend/tf_hub/object_detection/ssd_mobilenet_v2.py
@@ -2,10 +2,10 @@
 # https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1
 import tensorflow_hub as tf_hub
 
-from toy_backend.image.object_detection import ObjectDetection
+from toy_backend.tf_hub.model import ObjectDetectionModel
 
 
-class SsdMobilenetV2(ObjectDetection):
+class SsdMobilenetV2(ObjectDetectionModel):
     def __init__(self):
         super(SsdMobilenetV2, self).__init__()
 
@@ -13,3 +13,4 @@ class SsdMobilenetV2(ObjectDetection):
         if self.model is None:
             module_handle = "https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1"
             self.model = tf_hub.load(module_handle).signatures['default']
+            print(f'SsdMobilenetV2 - loaded model!')
