88b5f620a5d40863b4c7f0e250792aa38f0b122a
sweetjoohyun <joohyun@sqisoft.com>
2022-07-18 16:57:46 +0900

    object crop image 파일명 수정

--- toy_backend/image/image_helper.py
+++ toy_backend/image/image_helper.py
@@ -3,7 +3,6 @@ import cv2
 
 from toy_backend.common.util import is_file_exist
 
-
 TEXT_FONT = cv2.FONT_HERSHEY_SIMPLEX
 FONT_SIZE = 0.5
 
@@ -15,7 +14,7 @@ def crop_image(np_image, result, base_dir, file_name='0'):
     cropped_image = np_image[int(ymin*img_height):int(ymax*img_height),
                     int(xmin*img_width):int(xmax*img_width)]
 
-    result_saved_img_url = os.path.join(base_dir, file_name + '_' + entity+'_' + str(score) + '.jpg')
+    result_saved_img_url = os.path.join(base_dir, str(file_name) + '_' + entity+'_' + str(score) + '.jpg')
     cv2.imwrite(result_saved_img_url, cropped_image)
     new_coordinates = {'xmin': int(xmin*img_width),
                        'ymin': int(ymin*img_height),
