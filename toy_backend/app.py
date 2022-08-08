2139d91da37c4ab237d03d09a1e2d1d4c1d83253
honeybeeveloper <honeybeeveloper@naver.com>
2022-08-08 17:00:55 +0900

    add video play & video object detection api

--- toy_backend/app.py
+++ toy_backend/app.py
@@ -1,15 +1,14 @@
-from flask import Flask, request, jsonify, send_file
+import re
+import os
+from flask import Flask, request, jsonify, send_file, Response
 from flask_cors import CORS
 import traceback
 
 from toy_backend import logger
-from toy_backend.json_metadata import DetectionJson
+from toy_backend.common.exception import CustomException, FileNotFound
 from toy_backend.routes import image, video
-from toy_backend.common.exception import CustomException
 from toy_backend.tf_hub import Models, SsdMobilenetV2
-from toy_backend.image.image_helper import to_ndarray
-from toy_backend.tf_hub.object_detection.image_handler import ImageHandler
-from toy_backend.tf_hub.object_detection.video_handler import VideoHandler
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -36,59 +35,83 @@ def send_image():
         return send_file(path_or_file=r'D:\React\toy-front\public\images\no_image.jpg', mimetype='image/jpeg')
 
 
+@app.route(f'{api_root}/video')
+def send_video():
+    logger.debug(request)
+    try:
+        full_path = request.args.get('videoPath')
+        range_header = request.headers.get('Range', None)
+        byte1, byte2 = 0, None
+
+        if range_header:
+            match = re.search(r'(\d+)-(\d*)', range_header)
+            groups = match.groups()
+
+            if groups[0]:
+                byte1 = int(groups[0])
+            if groups[1]:
+                byte2 = int(groups[1])
+
+        chunk, start, length, file_size = get_chunk(full_path, byte1, byte2)
+        resp = Response(chunk, 206, mimetype='video/mp4', content_type='video/mp4', direct_passthrough=True)
+        resp.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(start, start + length - 1, file_size))
+        return resp
+    except FileNotFoundError:
+        raise FileNotFound('video file not found')
+
+
+def get_chunk(full_path, byte1=None, byte2=None):
+    file_size = os.stat(full_path).st_size
+    start = 0
+
+    if byte1 < file_size:
+        start = byte1
+    if byte2:
+        length = byte2 + 1 - byte1
+    else:
+        length = file_size - start
+
+    with open(full_path, 'rb') as f:
+        f.seek(start)
+        chunk = f.read(length)
+    return chunk, start, length, file_size
+
+
 @app.route(f'{api_root}/image/object-detection/ssdmobilenetv2', methods=['POST'])
 def image_object_detection_ssdmobilenet_v2():
     logger.debug(request)
     this_model_name = 'SsdMobilenetV2'
-    handler = ImageHandler(this_model_name, request.json['path'])
     if not model_manager.is_exists_model(this_model_name):
         model = SsdMobilenetV2()
         model_manager.register_model(model)
     else:
         model = model_manager.get_model(this_model_name)
 
-    # convert to ndarray
-    np_image = to_ndarray(request.json['path'])
-    # run detection
-    result = model.run_detector(np_image)
-    # handle result
-    final_result = handler.handle_detection(np_image, result)
-
+    json_path, final_result = image.ImageAPI.run_object_detection(model, request.json['path'])
     if final_result is None:
         return {'path': None, 'result': [{}]}
 
-    print({'path': handler.json_path, 'result': final_result})
-    return {'path': handler.json_path, 'result': final_result}
+    print({'path': json_path, 'result': final_result})
+    return {'path': json_path, 'result': final_result}
 
 
-import cv2
 @app.route(f'{api_root}/video/object-detection/ssdmobilenetv2', methods=['POST'])
 def video_object_detection_ssdmobilenet_v2():
     logger.debug(request)
     this_model_name = 'SsdMobilenetV2'
-    handler = VideoHandler(this_model_name, request.json['path'])
+
     if not model_manager.is_exists_model(this_model_name):
         model = SsdMobilenetV2()
         model_manager.register_model(model)
     else:
         model = model_manager.get_model(this_model_name)
 
-    v_cap = cv2.VideoCapture(filename=request.json['path'], apiPreference=cv2.CAP_FFMPEG)
-    frame_cnt = 0
-    final_result = {}
-    while v_cap.isOpened() and frame_cnt < 20:
-        ret, frame = v_cap.read()
-        if ret:
-            result = model.run_detector(frame)
-            handler.video_handler_init()
-            return_result = handler.handle_detection(frame_cnt, frame, result)
-            if return_result is not None:
-                final_result[frame_cnt] = return_result
-            frame_cnt += 1
+    json_path, final_result, fps = video.VideoAPI.run_object_detection(model, request.json['path'])
+    if final_result is None:
+        return {'path': None, 'result': [{}]}
 
-    print(final_result)
-    DetectionJson.save(out_path=handler.json_path, metadata=final_result)
-    return {'path': handler.json_path, 'result': [{}]}
+    print({'path': json_path, 'result': final_result, 'fps': fps})
+    return {'path': json_path, 'result': final_result, 'fps': fps}
 
 
 if __name__ == '__main__':
