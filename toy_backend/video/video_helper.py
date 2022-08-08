cd6b76add621e0ec199261d9630b2f5190badbfb
honeybeeveloper <honeybeeveloper@naver.com>
2022-08-08 17:00:27 +0900

    modify parameter name

--- toy_backend/video/video_helper.py
+++ toy_backend/video/video_helper.py
@@ -4,8 +4,8 @@ TEXT_FONT = cv2.FONT_HERSHEY_SIMPLEX
 FONT_SIZE = 0.5
 
 
-def video_info(input):
-    vid_cap = cv2.VideoCapture(input)
+def video_info(input_path):
+    vid_cap = cv2.VideoCapture(input_path)
     if vid_cap.isOpened():
         return {'fps': vid_cap.get(cv2.CAP_PROP_FPS),
                 'frame_width': int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
